afa6a9bdab7273efc2710923bc3126bfc3a208d7
Antonio Campos <tonyldo@gmail.com>
2018-12-10 19:50:19 -0300

    mosquitto docker compose file 1st version

--- mosquitto-entrypoint.sh
+++ mosquitto-entrypoint.sh
@@ -19,29 +19,23 @@ sed -i 's/#log_type information/#log_type information\nlog_type all/g' /mosquitt
 sed -i 's/#persistence false/persistence true/g' /mosquitto/config/mosquitto.conf
 sed -i 's/#persistence_location/persistence_location \/mosquitto\/data\//g' /mosquitto/config/mosquitto.conf
 
-echo "Setting bridge config for mosquitto..."
-sed -i 's/#connection <name>/connection ExternalBroker/g' /mosquitto/config/mosquitto.conf
-
-sed -i 's/#address <host>\[:<port>\] \[<host>\[:<port>\]\]/address $EXTERNAL_MQTT_SERVER:$EXTERNAL_MQTT_PORT/g' /mosquitto/config/mosquitto.conf
-sed -i 's/#topic <topic> \[\[\[out | in | both\] qos-level\] local-prefix remote-prefix\]/topic # both/g' /mosquitto/config/mosquitto.conf
-
-sed -i 's/#bridge_protocol_version mqttv311/bridge_protocol_version mqttv311/g' /mosquitto/config/mosquitto.conf
-
-sed -i 's/#cleansession false/cleansession true/g' /mosquitto/config/mosquitto.conf
-
-sed -i 's/#keepalive_interval 60/keepalive_interval 300/g' /mosquitto/config/mosquitto.conf
-
-sed -i 's/#local_clientid/local_clientid internalMosquitoMQTTBroker/g' /mosquitto/config/mosquitto.conf
-
-sed -i 's/#start_type automatic/start_type automatic/g' /mosquitto/config/mosquitto.conf
-
-sed -i 's/#remote_username/remote_username $EXTERNAL_MQTT_USERNAME/g' /mosquitto/config/mosquitto.conf
-
-sed -i 's/#remote_password/remote_password $EXTERNAL_MQTT_PASSWORD/g' /mosquitto/config/mosquitto.conf
-
-sed -i 's/#bridge_cafile/bridge_cafile \/etc\/ssl\/certs\/ca-certificates.crt/g' /mosquitto/config/mosquitto.conf
-
-sed -i 's/#bridge_insecure false/bridge_insecure false/g' /mosquitto/config/mosquitto.conf
+if ( [ -z "${EXTERNAL_MQTT_SERVER}" ] || [ -z "${EXTERNAL_MQTT_PORT}" ] || [ -z "${EXTERNAL_MQTT_USERNAME}" ] || [ -z "${EXTERNAL_MQTT_PASSWORD}" ]); then
+    echo "EXTERNAL_MQTT_SERVER or EXTERNAL_MQTT_PORT or EXTERNAL_MQTT_USERNAME or EXTERNAL_MQTT_PASSWORD not defined!"
+  else
+    echo "Setting bridge config for mosquitto..."
+    sed -i 's/#connection <name>/connection ExternalBroker/g' /mosquitto/config/mosquitto.conf
+    sed -i 's/#address <host>\[:<port>\] \[<host>\[:<port>\]\]/address '"$EXTERNAL_MQTT_SERVER"':'"$EXTERNAL_MQTT_PORT"'/g' /mosquitto/config/mosquitto.conf
+    sed -i 's/#topic <topic> \[\[\[out | in | both\] qos-level\] local-prefix remote-prefix\]/topic # both/g' /mosquitto/config/mosquitto.conf
+    sed -i 's/#bridge_protocol_version mqttv311/bridge_protocol_version mqttv311/g' /mosquitto/config/mosquitto.conf
+    sed -i 's/#cleansession false/cleansession true/g' /mosquitto/config/mosquitto.conf
+    sed -i 's/#keepalive_interval 60/keepalive_interval 300/g' /mosquitto/config/mosquitto.conf
+    sed -i 's/#local_clientid/local_clientid internalMosquitoMQTTBroker/g' /mosquitto/config/mosquitto.conf
+    sed -i 's/#start_type automatic/start_type automatic/g' /mosquitto/config/mosquitto.conf
+    sed -i 's/#remote_username/remote_username '"$EXTERNAL_MQTT_USERNAME"'/g' /mosquitto/config/mosquitto.conf
+    sed -i 's/#remote_password/remote_password '"$EXTERNAL_MQTT_PASSWORD"'/g' /mosquitto/config/mosquitto.conf
+    sed -i 's/#bridge_cafile/bridge_cafile \/etc\/ssl\/certs\/ca-certificates.crt/g' /mosquitto/config/mosquitto.conf
+    sed -i 's/#bridge_insecure false/bridge_insecure false/g' /mosquitto/config/mosquitto.conf
+fi
 
 cat /mosquitto/config/mosquitto.conf
 exec "$@"
